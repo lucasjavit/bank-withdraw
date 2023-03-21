ea2e0671c147790d3ef396a6b85418e3dcddeeca
vieiralucas89 <vyeiralucas@gmail.com>
2023-03-21 20:37:40 -0300

    :tada: first commit

---
+++ src/main/java/com/lucasit/withdraw/WithdrawApplication.java
@@ -0,0 +1,13 @@
+package com.lucasit.withdraw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithdrawApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithdrawApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lucasit/withdraw/WithdrawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasit.withdraw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithdrawApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
