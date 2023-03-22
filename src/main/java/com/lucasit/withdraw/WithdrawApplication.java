1ff9531e301c726e3d40331423b9d00dd084360d
vieiralucas89 <vyeiralucas@gmail.com>
2023-03-22 11:30:45 -0300

    :construction: add OperationTypeRepository

--- src/main/java/com/lucasit/withdraw/WithdrawApplication.java
+++ src/main/java/com/lucasit/withdraw/WithdrawApplication.java
@@ -1,13 +1,27 @@
 package com.lucasit.withdraw;
 
+import com.lucasit.withdraw.model.OperationType;
+import com.lucasit.withdraw.repository.OperationTypeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WithdrawApplication {
+@RequiredArgsConstructor
+public class WithdrawApplication implements ApplicationRunner {
+
+    private final OperationTypeRepository operationTypeRepository;
 
     public static void main(String[] args) {
         SpringApplication.run(WithdrawApplication.class, args);
     }
 
+    @Override
+    public void run(ApplicationArguments args) {
+        operationTypeRepository.save(new OperationType(1l, "TOP_UP"));
+        operationTypeRepository.save(new OperationType(2l, "WITHDRAW"));
+        operationTypeRepository.save(new OperationType(3l, "PAYMENT"));
+    }
 }
---
+++ src/main/java/com/lucasit/withdraw/repository/OperationTypeRepository.java
@@ -0,0 +1,9 @@
+package com.lucasit.withdraw.repository;
+
+import com.lucasit.withdraw.model.OperationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OperationTypeRepository extends JpaRepository<OperationType, Long> {
+}
